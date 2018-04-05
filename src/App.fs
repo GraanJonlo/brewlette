77ed271adec9cef8befded442d938164cf9f1866
Andy Grant <andy.a.grant@gmail.com>
2018-04-05 21:11:10 +0100

    Getting my thoughts in order

--- src/App.fs
+++ src/App.fs
@@ -3,17 +3,40 @@ module Brewlette
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Import.Browser
+
+type Rectangle = {
+    fill: U3<string,CanvasGradient,CanvasPattern>
+    width:float
+    height:float
+}
+
+let drawBg (canvas:HTMLCanvasElement) (ctx:CanvasRenderingContext2D) f =
+    ctx.fillStyle <- f
+    ctx.fillRect (0., 0., canvas.width, canvas.height)
+
+let drawRect (canvas:HTMLCanvasElement) (ctx:CanvasRenderingContext2D) rect x y =
+    ctx.fillStyle <- rect.fill
+    ctx.fillRect (x, y, rect.width, rect.height)
 
 let init() =
     let canvas = Browser.document.getElementsByTagName_canvas().[0]
     canvas.width <- 1000.
     canvas.height <- 800.
     let ctx = canvas.getContext_2d()
-    // The (!^) operator checks and casts a value to an Erased Union type
-    // See http://fable.io/docs/interacting.html#Erase-attribute
-    ctx.fillStyle <- !^"rgb(200,0,0)"
-    ctx.fillRect (10., 10., 55., 50.)
-    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
-    ctx.fillRect (30., 30., 55., 50.)
+
+    drawBg canvas ctx <| U3.Case1 "black"
+    let rect1 = {
+        fill = !^"rgb(200,0,0)"
+        width = 55.
+        height = 50.
+    }
+    let rect2 = {
+        fill = !^"rgba(0, 0, 200, 0.5)"
+        width = 55.
+        height = 50.
+    }
+    drawRect canvas ctx rect1 10. 10.
+    drawRect canvas ctx rect2 30. 30.
 
 init()
